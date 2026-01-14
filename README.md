1f7e4e85f64025dd58a3da21190b11e64434a8dd
skyman <skyman@skymandeMacBook-Pro.local>
2026-01-14 09:50:12 +0800

    feat: mcp example

---
+++ README.md
@@ -0,0 +1,10 @@
+# 安装uv
+
+# 从0开始
+
+## 安装依赖
+```shell
+uv init
+uv add pydantic-ai openai playwright python-dotenv schedule
+```
+
---
+++ demo_mcp/mcp_demo.py
@@ -0,0 +1,18 @@
+from mcp.server.fastmcp import FastMCP
+import mcp_tools
+
+mcp = FastMCP("host info mcp")
+mcp.add_tool(mcp_tools.get_host_info)
+
+@mcp.tool()
+def foo():
+    return ""
+
+def main():
+    mcp.run("stdio") # sse
+
+
+if __name__ == "__main__":
+    main()
+
+    
